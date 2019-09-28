e03a9d7918b3db5a75c6c6701945df9449054d8e
CapgProject <54829831+CapgProject@users.noreply.github.com>
2019-09-28 17:18:31 +0530

    Add files via upload
    changes

--- OnlineTestManagement/src/com/cg/testmanagement/controller/TestManagementController.java
+++ OnlineTestManagement/src/com/cg/testmanagement/controller/TestManagementController.java
@@ -1,10 +1,17 @@
 package com.cg.testmanagement.controller;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cg.testmanagement.dto.OnlineTest;
+import com.cg.testmanagement.exception.UserException;
 import com.cg.testmanagement.service.OnlineTestService;
 
 @Controller
@@ -15,6 +22,23 @@ public class TestManagementController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String displayHomePage() {
+		return "home";
+	}
+	@RequestMapping(value = "/addtest", method = RequestMethod.GET)
+	public String showAddTest(@ModelAttribute("test") OnlineTest test) {
+		return "AddTest";
+	}
+	@RequestMapping(value = "/addtestsubmit", method = RequestMethod.POST)
+	public String addTest(@ModelAttribute("test") OnlineTest test) {
+		try {
+			DateTimeFormatter durarationFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+			String timeString = test.getTestDuration().format(durarationFormatter);
+			LocalTime duration = LocalTime.parse(timeString, durarationFormatter);
+			test.setTestDuration(duration);
+			testservice.addTest(test);
+		} catch (UserException e) {
+			System.out.println(e.getMessage());
+		}
 		return "home";
 	}
 }
